64a0bcb41a1f81f4c8101b92b699613c534f4833
Elijah Zimmerman <elijahz0409@gmail.com>
2026-04-19 15:11:33 -0400

    Configure keychain dep

---
+++ src/tests/keychain_test.cc
@@ -0,0 +1,33 @@
+//  This file is part of Custodes SDC.
+//
+// Custodes SDC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Custodes SDC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Custodes SDC. If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+
+#include "keychain/keychain.h"
+
+TEST(KeychainTest, Integration) {
+    keychain::Error error;
+
+    const std::string package = "com.test.keychain-test";
+    const std::string service = "test";
+    const std::string user = "gtest";
+
+    keychain::setPassword(package, service, user, "testpass", error);
+    if (error) {
+        std::cout << error.message << std::endl;
+        FAIL();
+    }
+    auto password = keychain::getPassword(package, service, user, error);
+    ASSERT_EQ(password, "testpass");
+
+    keychain::deletePassword(package, service, user, error);
+    if (error) {
+        std::cout << error.message << std::endl;
+        FAIL();
+    }
+}
