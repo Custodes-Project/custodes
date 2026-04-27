6b7650e1e18ba38d061bf6cbe85a615ca73ac45f
Samuel Jones <sjones.nova@gmail.com>
2026-04-26 20:42:20 -0400

    add create salt func

---
+++ src/crypto.cpp
@@ -0,0 +1,34 @@
+//  This file is part of Custodes SDC.
+//
+// Custodes SDC is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Custodes SDC is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Custodes SDC. If not, see <https://www.gnu.org/licenses/>.
+//
+// Author: Samuel Jones
+// Created: April 26, 2026
+// Modified: April 17, 2026
+
+#include <sodium.h>
+#include <sodium/crypto_box.h>
+#include <sodium/crypto_pwhash.h>
+
+#include <memory>
+
+#include "./include/store.hpp"
+
+namespace custodes {
+Salt CreateSalt() {
+  Salt salt(new unsigned char[crypto_pwhash_SALTBYTES]);
+  randombytes_buf(salt.get(), crypto_pwhash_SALTBYTES);
+  return salt;
+}
+}  // namespace custodes
---
+++ src/include/crypto.hpp
@@ -0,0 +1,26 @@
+//  This file is part of Custodes SDC.
+//
+// Custodes SDC is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Custodes SDC is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Custodes SDC. If not, see <https://www.gnu.org/licenses/>.
+//
+// Author: Samuel Jones
+// Created: April 26, 2026
+// Modified: April 17, 2026
+
+#include <sodium.h>
+
+#include "./store.hpp"
+
+namespace custodes {
+Salt CreateSalt();
+}  // namespace custodes
